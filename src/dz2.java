d877dff8a560d3b80124bee37eb131b765112b82
unique2k18 <the_quemal@mail.ru>
2017-11-20 22:03:38 +0200

    написал программу 1

--- src/dz2.java
+++ src/dz2.java
@@ -4,7 +4,17 @@ public class dz2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите ваше имя: ");
-        String str = scanner.nextLine();
+        String name = scanner.nextLine();
+        System.out.println("Введите ваш город: ");
+        String city = scanner.nextLine();
+        System.out.println("Введите ваш возраст: ");
+        int age = scanner.nextInt();
+        System.out.println("Введите ваше хобби: ");
+        String hobby = scanner.nextLine();
+        System.out.println("Имя: " + name);
+        System.out.println("Город: " +city);
+        System.out.println("Возраст: " + age);
+        System.out.println("Хобби: " + hobby);
 
     }
 }
