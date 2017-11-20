8984352b8a4d528fc6437baa1d1ffd7d4a929725
unique2k18 <the_quemal@mail.ru>
2017-11-20 21:50:08 +0200

    Ввел строку, которая спрашивает мое имя.

---
+++ src/dz2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class dz2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ваше имя: ");
+        String str = scanner.nextLine();
+
+    }
+}
